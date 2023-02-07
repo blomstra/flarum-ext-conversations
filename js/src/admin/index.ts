93166b97c1f7d5b625c8a6582fe03e2d62ac3540
Ian Morland <ian@morland.me>
2023-02-07 10:23:30 +0000

    chore: update build.yml, npm audit fix, format js

--- js/src/admin/index.ts
+++ js/src/admin/index.ts
@@ -1,9 +1,13 @@
 import app from 'flarum/admin/app';
 
 app.initializers.add('blomstra/conversations', () => {
-  app.extensionData.for('blomstra-conversations').registerPermission({
-    icon: 'fas fa-bullseye',
-    label: app.translator.trans('blomstra-conversations.admin.permissions.view-conversation-source'),
-    permission: 'discussion.viewConversationSource',
-  }, 'moderate', 55)
+  app.extensionData.for('blomstra-conversations').registerPermission(
+    {
+      icon: 'fas fa-bullseye',
+      label: app.translator.trans('blomstra-conversations.admin.permissions.view-conversation-source'),
+      permission: 'discussion.viewConversationSource',
+    },
+    'moderate',
+    55
+  );
 });
